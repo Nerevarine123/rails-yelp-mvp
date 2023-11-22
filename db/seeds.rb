b01f77ee77396389eac8f7d7e0d41b8370311a73
Usman Ali <usmanv5@hotmail.com>
2023-11-22 11:31:10 +0000

    scheme/model and seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Restaurant.create(name: 'griller', address: 'london', phone_number: '6969', category: 'french')
+Restaurant.create(name: 'franzos', address: 'berlin', phone_number: '6969420', category: 'belgian')
+Restaurant.create(name: 'chinatown', address: 'hongkong', phone_number: '1234', category: 'chinese')
+Restaurant.create(name: 'nandos', address: 'rome', phone_number: '696942012', category: 'italian')
+Restaurant.create(name: 'ramenplace', address: 'tokyo', phone_number: '1234952', category: 'japanese')
