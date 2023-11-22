79448695ff41b6581b5e6aef1cb6c58e5af80ed3
Usman Ali <usmanv5@hotmail.com>
2023-11-22 12:07:03 +0000

    amended refactored

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,16 +1,15 @@
 class RestaurantsController < ApplicationController
-  # before_action :set_restaurant, only: [:show]
+  before_action :set_restaurant, only: [:show]
   def index
     @restaurants = Restaurant.all
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
   end
 
   private
 
-  # def set_restaurant
-  #  @restaurant = Restaurant.find(params[:id])
-  # end
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
